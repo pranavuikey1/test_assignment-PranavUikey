696f721091ea0c1ea6dc9ae83245f220acd7e5d0
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2022-03-22 17:05:16 +0530

    checking 2 test case

--- test_hello.py
+++ test_hello.py
@@ -3,4 +3,4 @@ import hello;
 def test_hello():
     print('It is working.')
     assert hello.hello_world() == "Hello World!"
-    assert "Hello World!" == hello.hello_world()
+    assert "Hello!" == hello.hello_world()
