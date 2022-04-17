19758cf3acd0d17c0ef6952fb5320273a39e4f79
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2022-04-17 11:47:21 +0530

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,6 +1,6 @@
-import hello;
+import hello
 
 def test_hello():
     print('It is working.')
     assert hello.hello_world() == "Hello World!"
-    assert "Hello!" == hello.hello_world()
+    assert "Hello World!" == hello.hello_world()
